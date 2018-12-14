b4c56695f74bc98863edaf31c89a2440fd278cff
Sandy Armstrong <sandy@xamarin.com>
2018-12-14 14:19:59 -0800

    SurpriseWitness: Add Xamarin.Mac.Sdk version of WitnessMe to demo bug

---
+++ SurpriseWitness/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace SurpriseWitness
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // In a Xamarin.Mac.Sdk project, this will throw because the ref
+            // assembly got bundled instead of the proper one from lib in the
+            // System.Memory NuGet package.
+            var x = System.Buffers.ReadOnlySequence<int>.Empty;
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
